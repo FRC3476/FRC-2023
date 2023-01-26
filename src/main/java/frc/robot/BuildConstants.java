2fa8bf1ee9ab6ed8b0b937a7061fc78fb60617bb
varun7654 <varun7654@outlook.com>
2023-01-26 14:12:49 -0800

    add the advantage kit library

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "";
+    public static final String VERSION = "";
+    public static final int GIT_REVISION = 0;
+    public static final String GIT_SHA = "";
+    public static final String GIT_DATE = "";
+    public static final String GIT_BRANCH = "";
+    public static final String BUILD_DATE = "";
+    public static final long BUILD_UNIX_TIME = 0;
+    public static final int DIRTY = 1;
+
+    private BuildConstants() {}
+}
