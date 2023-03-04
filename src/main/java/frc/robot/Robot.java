06b79665c93bd2eb0a15f7abbeaab024896cb4de
RohanP321 <120353864+Rohan3476@users.noreply.github.com>
2023-03-04 14:22:39 -0800

    resets absolute zeroes without reformatting code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -181,6 +181,7 @@ public class Robot extends LoggedRobot {
             elevator = new Elevator(new ElevatorIO() {});
             telescopingArm = new TelescopingArm(new TelescopingArmIO() {});
             grabber = new Grabber(new GrabberIO() {});
+            drive.resetAbsoluteZeros();
         }
 
         Logger.getInstance().start(); // Start logging! No more data receivers, replay sources, or metadata values may be added
