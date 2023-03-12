bd428b1035434e992433a94f9706c33a6d46059c
varun7654 <varun7654@outlook.com>
2023-03-12 10:16:11 -0700

    move upper scoring position up half an inch

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -288,7 +288,7 @@ public final class Constants {
     public static final Rotation2d PICKUP_ANGLE_BLUE = Rotation2d.fromDegrees(180);
 
     public static final double LOWER_PICKUP_POSITION_Y = -3.56;
-    public static final double UPPER_PICKUP_POSITION_Y = -2.05892 + Units.inchesToMeters(0.5);
+    public static final double UPPER_PICKUP_POSITION_Y = -2.05892 + Units.inchesToMeters(1);
     public static final double PICKUP_POSITION_X_OFFSET_FROM_WALL = FIELD_WIDTH_METERS - 15.2 + Units.inchesToMeters(-1);
 
     public static final double SCORING_POSITION_OFFSET_CONE_FROM_WALL = 0.05;
